657cc590bd066234b7d1286db6dd6390b14d45d8
khizar <a.khizar@p3fusion.com>
2022-09-05 23:55:24 +0530

    till task screen

--- src/dashboard/pages/newinteraction.js
+++ src/dashboard/pages/newinteraction.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useLayoutEffect, useEffect } from 'react';
-import { Button, Input, PageHeader, Row, Tabs, Menu, Radio, Col, Card, Space, Collapse, Modal, Form, Divider, Steps } from 'antd';
+import { Button, Input, PageHeader, Row, Tabs, Menu, Radio, Col, Card, Space, Collapse, Modal, Form, Divider, Steps, notification, Spin } from 'antd';
 import { PlusCircleOutlined, SettingOutlined, MinusSquareOutlined, InsuranceOutlined, PropertySafetyOutlined, CloseOutlined } from '@ant-design/icons';
 import '../assets/style/interaction.less'
 import AddBuilding from './addBuilding';
@@ -24,7 +24,26 @@ const NewInteraction = (props) => {
             loh: false,
         }
     })
-
+    const tasksEndpoints=[
+        {
+            "endpointARN": "arn:aws:connect:us-east-1:851171462885:instance/bc83e5db-b0ea-4c92-a4df-5b7d994869fa/transfer-destination/73174441-536b-48a7-87dd-ff45406eee52",
+            "endpointId": "arn:aws:connect:us-east-1:851171462885:instance/bc83e5db-b0ea-4c92-a4df-5b7d994869fa/transfer-destination/73174441-536b-48a7-87dd-ff45406eee52",
+            "type": "agent",
+            "name": "Jim",
+            "phoneNumber": null,
+            "agentLogin": null,
+            "queue": null
+        },
+        {
+            "endpointARN": "arn:aws:connect:us-east-1:851171462885:instance/bc83e5db-b0ea-4c92-a4df-5b7d994869fa/transfer-destination/245c9d61-c8fc-4f4e-9414-ea94b22c52e9",
+            "endpointId": "arn:aws:connect:us-east-1:851171462885:instance/bc83e5db-b0ea-4c92-a4df-5b7d994869fa/transfer-destination/245c9d61-c8fc-4f4e-9414-ea94b22c52e9",
+            "type": "agent",
+            "name": "Supervisor",
+            "phoneNumber": null,
+            "agentLogin": null,
+            "queue": null
+        }
+    ]
     useEffect(() => {
         const connectUrl = "https://p3fusion-uat.my.connect.aws/ccp-v2"
 
@@ -116,29 +135,79 @@ const NewInteraction = (props) => {
     }, [])
 
     const searchProfile = () => {
-        const instanceUrl = 'https://p3fusion-uat.my.connect.aws/'
-        let client = new connect.CustomerProfilesClient(instanceUrl);
 
         if (connect.agent.initialized) {
             connect.agent(function (agent) {
-                const queryParams = {// required
-                   
-                    maxResults: 50 //optional, number, max value of 100
+
+               /*  const templateParams = {// required
+                    id: '0db3b826-c940-4611-b8b3-dd54e104fe9c', //required, string, template ID, template should belong to connect instance                  
                 };
+
+                agent.getTaskTemplate(templateParams, {
+                    success: function (data) {
+                        console.log("Template data loaded successfully")
+                        console.log({ templateParams, data })
+                    },
+                    failure: function (err) { console.error({ err }) }
+                }); */
+
+                const newTask = {
+                    name: "string", //required, max len: 512
+                    description: "string", //optional, max len: 4096
+                    endpoint: tasksEndpoints[0], //required for non templated tasks, can be retrieved via `agent.getEndpoints()`. Agent and queue endpoints supported.
+                    taskTemplateId: "string", //required for templated tasks, ID of the template the task is created from. Template should belong to connect instance
+                    previousContactId: "string", //optional, the previous contact ID for a linked task
+                    references: { //optional. Only URL references are supported for non templated tasks
+                        "reference name 1": { // string, max len: 4096
+                            type: "URL", //required, string, one of connect.ReferenceType types, 
+                            value: "https://www.amazon.com" //required, string, max len: 4096
+                        },
+                        "reference name 2": { // string, max len: 4096
+                            type: "EMAIL", //required, string, one of connect.ReferenceType types
+                            value: "example@abc.com" //required, string, max len: 4096
+                        },	
+                        "reference name 3": { // string, max len: 4096
+                            type: "NUMBER", //required, one of connect.ReferenceType types
+                            value: 1000 //required, number
+                        },
+                        "reference name 4": { // string, max len: 4096
+                            type: "DATE", //required, string, one of connect.ReferenceType types
+                            value: 1649961230 //required, number
+                        },
+                        "reference name 5": { // string, max len: 4096
+                            type: "STRING", //required, string, one of connect.ReferenceType types
+                            value: "example@abc.com" //required, string, max len: 4096
+                        }	
+                    },
+                   
+                };//ends newTask
+                console.log(newTask);
+                agent.createTask(newTask, {
+                    success: function (data) { 
+                        console.log("Created a task with contact id: ", data.contactId) 
+                    },
+                    failure: function (err) { 
+                        console.log("error has occured",err); 
+                    }
+                });//ends createT
+
+                /*  const queryParams = {// required                   
+                     maxResults: 50 //optional, number, max value of 100
+                 };
+                 
+                 agent.listTaskTemplates(queryParams, {
+                     success: function(data) { 
+                         console.log({data});
+                      },
+                     failure: function(err) {console.error({err}); }
+                 });
+                 */
                 
-                agent.listTaskTemplates(queryParams, {
-                    success: function(data) { 
-                        console.log({data});
-                     },
-                    failure: function(err) {console.error({err}); }
-                });
-
-
-                /* var queuesARNs = agent.getAllQueueARNs();
+               /*  var queuesARNs = agent.getAllQueueARNs();
                 agent.getEndpoints(queuesARNs,{
                         success: function (data) {
                             var endpoints = data.endpoints; // or data.addresses
-                            console.log({data});
+                            console.log({data,endpoints});                           
                             const newTask = {
                                 name: "Test task from taskJS", //required, max len: 512
                                 description: "N/A", //optional, max len: 4096
@@ -158,14 +227,16 @@ const NewInteraction = (props) => {
                                 failure: function (err) { 
                                     console.log("error has occured",err); 
                                 }
-                            });//ends createTask
-                        },//end success
+                            });//ends createTask 
 
+
+                        },//end success
                         failure: function (err) {
                             console.error({err})
                         }
                     }//ends QueueARN
-                );// ends agent.getEndpoints */
+                );// ends agent.getEndpoints  */
+                
             })
 
             /*  client.createProfile({
@@ -223,53 +294,26 @@ export default NewInteraction
 const CustomerProile = (props) => {
     const customerprofiles = useRef(null);
     const { id } = props
+    const [loading, setLoading] = useState(false)
     useEffect(() => {
         if (customerprofiles.current) {
             const instanceUrl = 'https://p3fusion-uat.my.connect.aws/'
             let client = new connect.CustomerProfilesClient(instanceUrl);
         }
-    }, [])
+        if (connect.agent.initialized) {
+            notification.success({
+                message: "Customer Profile initiated successfully"
+            })
+            setLoading(true)
+        }
 
+    }, [connect.agent.initialized])
 
-    /*   useLayoutEffect(() => {
-          if (connect.agent.initialized) {
-  
-              connect.contact(function (contact) {
-                  contact.onIncoming(function (contact) {
-                      console.log("onIncoming", contact);
-                  });
-  
-                  contact.onRefresh(function (contact) {
-                      console.log("onRefresh", contact);
-                  });
-  
-                  contact.onAccepted(function (contact) {
-                      console.log("onAccepted", contact);
-                  });
-  
-                  contact.onEnded(function () {
-                      console.log("onEnded", contact);
-                  });
-  
-                  contact.onConnected(function () {
-                      console.log(`onConnected(${contact.getContactId()})`);
-                      var attributeMap = contact.getAttributes();
-                       var name = JSON.stringify(attributeMap["firstName"]["value"]);
-                       var phone = JSON.stringify(attributeMap["phoneNumber"]["value"]);
-                       var account = JSON.stringify(attributeMap["accountNumber"]["value"]);
-                       console.log(name);
-                       console.log(phone);
-                    
-                      console.log({
-                          attributeMap: JSON.stringify(attributeMap)
-                      })
-                  });
-              });
-          }
-  
-      }) */
 
     return (
-        <div id={`customerprofiles-container`} style={{ minHeight: '100vh' }} ref={customerprofiles} />
+        <section style={{ minHeight: '100vh' }}>           
+
+            <div id={`customerprofiles-container`} style={{ minHeight: '100vh' }} ref={customerprofiles} />
+        </section>
     )
 }
--- src/gc-components/payload.js
+++ src/gc-components/payload.js
@@ -1,3 +1,53 @@
 export const createProfile=()=>{
     return {"FirstName":"Khizar ","LastName":"Ahmed","Gender":"MALE","PartyType":"INDIVIDUAL","BirthDate":"1986-09-07","AccountNumber":"9715463636","EmailAddress":"khizaras@gmail.com","MailingAddress":{"Address1":"102","Address2":"big street","Country":"IN","PostalCode":"600005","City":"Chennai","Province":"Tamil Nadu"},"PhoneNumber":"+919715463636","DomainName":"amazon-connect-P3fusion-uat"}
 }
+
+const tasksEndpoints=[
+    {
+        "endpointARN": "arn:aws:connect:us-east-1:851171462885:instance/bc83e5db-b0ea-4c92-a4df-5b7d994869fa/transfer-destination/73174441-536b-48a7-87dd-ff45406eee52",
+        "endpointId": "arn:aws:connect:us-east-1:851171462885:instance/bc83e5db-b0ea-4c92-a4df-5b7d994869fa/transfer-destination/73174441-536b-48a7-87dd-ff45406eee52",
+        "type": "agent",
+        "name": "Jim",
+        "phoneNumber": null,
+        "agentLogin": null,
+        "queue": null
+    },
+    {
+        "endpointARN": "arn:aws:connect:us-east-1:851171462885:instance/bc83e5db-b0ea-4c92-a4df-5b7d994869fa/transfer-destination/245c9d61-c8fc-4f4e-9414-ea94b22c52e9",
+        "endpointId": "arn:aws:connect:us-east-1:851171462885:instance/bc83e5db-b0ea-4c92-a4df-5b7d994869fa/transfer-destination/245c9d61-c8fc-4f4e-9414-ea94b22c52e9",
+        "type": "agent",
+        "name": "Supervisor",
+        "phoneNumber": null,
+        "agentLogin": null,
+        "queue": null
+    }
+]
+const taskTemplate=[
+    {
+        "Arn": "arn:aws:connect:us-east-1:851171462885:instance/bc83e5db-b0ea-4c92-a4df-5b7d994869fa/task-template/0d82df03-9a49-43d6-a38c-707075e424e0",
+        "CreatedTime": 1661515621.589,
+        "Description": "Loss History",
+        "Id": "0d82df03-9a49-43d6-a38c-707075e424e0",
+        "LastModifiedTime": 1661515621.589,
+        "Name": "Loss History",
+        "Status": "ACTIVE"
+    },
+    {
+        "Arn": "arn:aws:connect:us-east-1:851171462885:instance/bc83e5db-b0ea-4c92-a4df-5b7d994869fa/task-template/0f6dbec0-4caa-4e25-bd61-1cef93899acb",
+        "CreatedTime": 1661515495.04,
+        "Description": "Address Change",
+        "Id": "0f6dbec0-4caa-4e25-bd61-1cef93899acb",
+        "LastModifiedTime": 1661515495.04,
+        "Name": "Address Change",
+        "Status": "ACTIVE"
+    },
+    {
+        "Arn": "arn:aws:connect:us-east-1:851171462885:instance/bc83e5db-b0ea-4c92-a4df-5b7d994869fa/task-template/0db3b826-c940-4611-b8b3-dd54e104fe9c",
+        "CreatedTime": 1661515344.353,
+        "Description": "Add additional building to existing insurance",
+        "Id": "0db3b826-c940-4611-b8b3-dd54e104fe9c",
+        "LastModifiedTime": 1661515344.353,
+        "Name": "Add Building",
+        "Status": "ACTIVE"
+    }
+]
