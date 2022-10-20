f29176e657f14a030bb7b3ebc1946ec1c45ed15e
Karthic Eswaran <rpdhivya@gmail.com>
2022-10-20 12:39:28 +0530

    steps wizard

--- src/onboarding/pages/onboard-page.js
+++ src/onboarding/pages/onboard-page.js
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout } from 'antd';
 import OnBoardSidebar from '../layout/siderbar';
 import OnBoardHeader from '../layout/header';
 import { Content } from 'antd/lib/layout/layout';
 import OnBoardSteps from './steps/steps';
-
+import { useSelector } from 'react-redux';
+import { navigate } from '@reach/router';
 
 const OnBoardPage = () => {
+
+  const user = useSelector((state) => state.user)
+  
+  useEffect(() => {
+    if(!user.AppAuth.isLoggedin)
+    {
+      navigate("/signup")
+    }
+  }, []);
 
   const [collapsed, setCollapsed] = useState(true);
 
@@ -15,7 +25,7 @@ const OnBoardPage = () => {
       <OnBoardSidebar collapsed={collapsed} />
       <Layout className="site-layout">
         <OnBoardHeader setCollapsed={setCollapsed} collapsed={collapsed} />
-        <Content className="main" style={{ width:'95%',height:'100%', margin: '24px 16px 24px 16px', backgroundColor: '#fafafa', border:'1px dashed #e9e9e9', borderRadius: '2px'  }}>
+        <Content className="main" style={{ margin: '24px 16px 24px 16px' }}>
           <div
             className="site-layout-background"
             style={{
@@ -25,7 +35,6 @@ const OnBoardPage = () => {
           >
             <OnBoardSteps/>
           </div>
-
         </Content>
       </Layout>
     </Layout>
