b52fb4b86c69eea8b59f5f4eeb072c5764c5c63b
Khizar <a.khizar@p3fusion.com>
2022-10-28 00:41:42 +0530

    modified signup flows

--- src/onboarding/pages/steps/chooseChannel.js
+++ src/onboarding/pages/steps/chooseChannel.js
@@ -1,90 +1,176 @@
-import { Checkbox, Radio, Form, Button, Space } from 'antd';
+import { Checkbox, Radio, Form, Button, Space, Row, Col, Typography, Divider, Collapse, Card, Switch, Input } from 'antd';
 import React, { useState } from 'react';
 
-
+import { PhoneOutlined, MailOutlined, MessageOutlined, CheckOutlined, CloseOutlined } from '@ant-design/icons';
 const CheckboxGroup = Checkbox.Group;
 const plainOptions = ['Phone', 'Chat', 'Email'];
 const defaultCheckedList = ['Phone'];
+import ils1 from '../../assets/images/illustrations/signup-4.svg'
+const { Panel } = Collapse;
 
 const ChooseChannel = (props) => {
 
-    const [value, setValue] = useState(1);
-    const [checkedList, setCheckedList] = useState(defaultCheckedList);
-    const [isSelectPhone, setIsSelectPhone] = useState(true);
+    const [state, setState] = useState({
+        channelPhone: true,
+        channelEmail: false,
+        channelChat: false,
+        phone: {
+            showOption: false,
+            showSubOption: false,
+            phoneNumberOptions: [
+                { label: 'Use my old phone number', value: 'oldNumber' },
+                { label: 'Allocate new number', value: 'newNumber' }
+            ],
+            phoneNumberSubOptions: [
+                { label: 'TFN', value: 'TFN' },
+                { label: 'DID', value: 'DID' }
+            ]
+        },
+        email: {},
+        chat: {},
+    })
 
-    const onChange = (list) => {
-        setCheckedList(list);
-        if (list.some(val => (val === 'Phone'))) {
-            setIsSelectPhone(true);
+    const onChangePhoneConfigure = (e) => {
+        if (e.target.value === 'newNumber') {
+            setState({
+                ...state,
+                phone: {
+                    ...state.phone,
+                    showOption: true,
+                }
+            })
         }
         else {
-            setIsSelectPhone(false);
-        }
-    };
+            setState({
+                ...state,
+                phone: {
+                    ...state.phone,
+                    showOption: false,
+                    showSubOption: false
 
-    const options = [
-        { label: 'Inbound', value: 'inbound' },
-        { label: 'Outbound', value: 'outbound' }
-    ];
-
-    const onChangeRadio = (e) => {
-        setValue(e.target.value);
+                }
+            })
+        };
     }
-
-    const onFinish = (values) => {
-        props.chooseChannel(values);
-        props.next();
-    };
-
-    const onPrev = () => {
-        props.prev();
-    }
-
-    const onFinishFailed = (errorInfo) => {
-        console.log('Failed:', errorInfo);
-    };
+    const onFinish = () => { }
+    const onFinishFailed = () => { }
 
 
     return (
-        <Form
-            name="step2"
-            layout='vertical'
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 14 }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-        >
+        <div className='steps-channel'>
+            <Row gutter={[16, 16]} align='stretch' justify='space-evenly'>
 
-            <Form.Item
-                initialValue={['Phone']}
-                label="Select channel"
-                name="channel"
-                rules={[{ required: true, message: 'Please select channel!' }]}
-            >
-                <CheckboxGroup options={plainOptions} value={checkedList} onChange={onChange} style={{ display: 'flex', flexDirection: 'column' }} />
-            </Form.Item>
-            {isSelectPhone &&
-            <Form.Item
-                label="Select phone type"
-                name="phoneType"
-                rules={[{ required: true, message: 'Please select type!' }]}
-            >
-                 <Radio.Group onChange={onChangeRadio} value={value} optionType='button' options={options} />
-            </Form.Item>
-            }
-            <Form.Item>
-                <Space>
-                <Button type="primary" htmlType="submit" >
-                    Next
-                </Button>
-                <Button type="secondary" htmlType="button" onClick={onPrev}>
-                    Previous
-                </Button>
-                </Space>
-            </Form.Item>
+                <Col span={8}>
+                    <img src={ils1} className="sideimg" />
+                </Col>
 
-        </Form>
+                <Col span={16}>
+                    <Typography.Title level={3}>Choose Your required Channels</Typography.Title>
+                    <Divider />
+                    <Row>
+                        <Col span={24}>
+                            <div className='channels'>
+                                <Card className='channel-item'>
+                                    <Row align='middle'>
+                                        <Col span={12}>
+                                            <PhoneOutlined style={{ fontSize: 50 }} />
+                                        </Col>
+                                        <Col span={12}>
+                                            <Typography.Title level={3}>Phone</Typography.Title>
+                                            <Switch
+                                                onChange={() => setState({ ...state, channelEmail: true })}
+                                                defaultChecked={state.channelPhone}
+                                            />
+                                        </Col>
+                                    </Row>
+                                </Card>
+                                <Card className='channel-item'>
+                                    <Row align='middle'>
+                                        <Col span={12}>
+                                            <MailOutlined style={{ fontSize: 50 }} />
+                                        </Col>
+                                        <Col span={12}>
+                                            <Typography.Title level={3}>Email</Typography.Title>
+                                            <Switch
+                                                onChange={() => setState({ ...state, phone: true })}
+                                                defaultChecked={state.channelEmail}
+                                            />
+                                        </Col>
+                                    </Row>
+                                </Card>
+                                <Card className='channel-item'>
+                                    <Row align='middle'>
+                                        <Col span={12}>
+                                            <MessageOutlined style={{ fontSize: 50 }} />
+                                        </Col>
+                                        <Col span={12}>
+                                            <Typography.Title level={3}>Chat</Typography.Title>
+                                            <Switch
+                                                onChange={() => setState({ ...state, channelChat: true })}
+                                                defaultChecked={state.channelChat}
+                                            />
+                                        </Col>
+                                    </Row>
+                                </Card>
+                            </div>
+                        </Col>
+                    </Row>
+
+                    <Row style={{ marginTop: 30 }}>
+                        <Col span={24}>
+                            <Collapse defaultActiveKey={['phone']} ghost >
+                                {state.channelPhone &&
+                                    <Panel header="Phone" key="phone" showArrow={false} >
+                                        <Form
+                                            name="phoneConfigure"
+                                            layout='vertical'
+                                            labelCol={{ span: 8 }}
+                                            wrapperCol={{ span: 14 }}
+                                            onFinish={onFinish}
+                                            onFinishFailed={onFinishFailed}
+                                            autoComplete="off"
+                                        >
+                                            <Form.Item initialValue={'oldNumber'} label='Select number' name='phoneNumberType' rules={[{ required: true, message: 'Please select any one number' }]}>
+                                                <Space><Radio.Group onChange={onChangePhoneConfigure} value={1} name='phoneNumberRadio' options={state.phone.phoneNumberOptions} optionType='button' /></Space>
+                                            </Form.Item>
+                                            {!state.phone.showOption &&
+                                                <Form.Item label='Enter your number' name='oldPhoneNumber' rules={[{ required: true, message: 'Please input your old number' }]} style={{ marginTop: '20px' }}>
+                                                    <Input />
+                                                </Form.Item>
+                                            }
+                                            {state.phone.showOption &&
+                                                <Form.Item label='Choose option' name='newNumberSubOption' rules={[{ required: true, message: 'Please select any one method' }]} style={{ marginTop: '20px' }}>
+                                                    <Space><Radio.Group onChange={onChangeSubValue} value={subValue} name='phoneNumberSubRadio' options={state.phone.phoneNumberSubOptions} optionType='button' /></Space>
+                                                </Form.Item>
+                                            }
+                                            {state.phone.showSubOption &&
+                                                <Form.Item label='Number of TFN required' name='noOfTFN' rules={[{ required: true, message: 'Please input your required number of TFN' }]} style={{ marginTop: '20px' }}>
+                                                    <Input />
+                                                </Form.Item>
+                                            }
+                                            <Form.Item>
+                                                <Button type="default" htmlType="submit" size='small'>
+                                                    Save
+                                                </Button>
+                                            </Form.Item>
+                                        </Form>
+                                    </Panel>
+                                }
+                                <Panel header="E-mail" key="2">
+                                    E-mail
+                                </Panel>
+                                <Panel header="Chat" key="3">
+                                    Calls
+                                </Panel>
+                            </Collapse>
+                        </Col>
+                    </Row>
+
+                </Col>
+            </Row>
+
+
+        </div>
     );
 };
 
--- src/onboarding/pages/steps/selectAccount.js
+++ src/onboarding/pages/steps/selectAccount.js
@@ -1,9 +1,9 @@
-import { Radio } from 'antd';
-import React, { useState } from 'react';
-import { Form, Input, Button } from 'antd';
 
+import { Button, Col, Divider, Form, Input, Radio, Row, Typography } from 'antd';
+import React, { useState } from 'react';
+import ils1 from '../../assets/images/illustrations/signup-2.svg'
 const SelectAccount = (props) => {
-    
+
     const [value, setValue] = useState('Self Managed AWS account');
     const options = [
         { label: 'Self Managed AWS account', value: 'Self Managed AWS account' },
@@ -20,7 +20,7 @@ const SelectAccount = (props) => {
     };
 
     const onFinish = (values) => {
-        let data = {...values, accountType:showOption?'Self Managed AWS account':'Presolved Managed AWS account'}
+        let data = { ...values, accountType: showOption ? 'Self Managed AWS account' : 'Presolved Managed AWS account' }
         props.selectAccount(data);
         props.next();
     };
@@ -36,53 +36,67 @@ const SelectAccount = (props) => {
                 padding: '15px',
             }}
         >
-            <Radio.Group onChange={onChange} value={value} options={options} optionType="button" />
-            <div style={{ justify: 'start', alignContent: 'center', margin: '25px 0 25px 0' }}>
-                <Form
-                    name="step1"
-                    layout='vertical'
-                    labelCol={{ span: 8 }}
-                    wrapperCol={{ span: 14 }}
-                    onFinish={onFinish}
-                    onFinishFailed={onFinishFailed}
-                    autoComplete="off"
-                >
-                    {showOption &&
-                        <Form.Item
-                            label="Account ID"
-                            name="selfAccountId"
-                            rules={[{ required: true, message: 'Please input your account id!' }]}
-                        >
-                            <Input />
-                        </Form.Item>
-                    }
-                    {!showOption &&
-                        <Form.Item
-                            label="Account Name"
-                            name="presolvedAccountName"
-                            rules={[{ required: true, message: 'Please input your Account Name!' }]}
-                        >
-                            <Input />
-                        </Form.Item>
 
-                    }
-                    {!showOption &&
-                        <Form.Item
-                            label="Email Id"
-                            name="presolvedEmailId"
-                            rules={[{ required: true, message: 'Please input your email id!' }]}
-                        >
-                            <Input />
-                        </Form.Item>
-                    }
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit">
-                            Next
-                        </Button>
-                    </Form.Item>
+            <Row gutter={[16, 16]} align='stretch' justify='space-evenly'>
 
-                </Form>
-            </div>
+                <Col span={8}>
+                    <img src={ils1} className="sideimg" />
+                </Col>
+                <Col span={16}>
+                    <Typography.Title level={3}>Please choose the Account type</Typography.Title>
+                    <Divider />
+                    <Radio.Group onChange={onChange} value={value} options={options} optionType="button" />
+                    <div style={{ justify: 'start', alignContent: 'center', margin: '25px 0 25px 0' }}>
+                        <Form
+                            name="step1"
+                            layout='vertical'
+                            labelCol={{ span: 8 }}
+                            wrapperCol={{ span: 14 }}
+                            onFinish={onFinish}
+                            onFinishFailed={onFinishFailed}
+                            autoComplete="off"
+                        >
+                            {showOption &&
+                                <Form.Item
+                                    label="Account ID"
+                                    name="selfAccountId"
+                                    rules={[{ required: true, message: 'Please input your account id!' }]}
+                                >
+                                    <Input />
+                                </Form.Item>
+                            }
+                            {!showOption &&
+                                <Form.Item
+                                    label="Account Name"
+                                    name="presolvedAccountName"
+                                    rules={[{ required: true, message: 'Please input your Account Name!' }]}
+                                >
+                                    <Input />
+                                </Form.Item>
+
+                            }
+                            {!showOption &&
+                                <Form.Item
+                                    label="Email Id"
+                                    name="presolvedEmailId"
+                                    rules={[{ required: true, message: 'Please input your email id!' }]}
+                                >
+                                    <Input />
+                                </Form.Item>
+                            }
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit">
+                                    Next
+                                </Button>
+                            </Form.Item>
+
+                        </Form>
+                    </div>
+                </Col>
+            </Row>
+
+
+
         </div>
 
     );
